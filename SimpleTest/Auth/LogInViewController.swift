def4419416c182449136287142ad98b16a6ba75e
YupengLin <linyupeng2400@hotmail.com>
2018-07-03 15:55:59 -0700

    login view

---
+++ SimpleTest/Auth/LogInViewController.swift
@@ -0,0 +1,44 @@
+//
+//  LogInViewController.swift
+//  SimpleTest
+//
+//  Created by Yupeng Lin on 7/3/18.
+//  Copyright © 2018 vluxe. All rights reserved.
+//
+
+import UIKit
+
+class LogInViewController: UIViewController {
+
+    @IBOutlet weak var email: UITextField!
+    @IBOutlet weak var password: UITextField!
+    
+    
+    @IBAction func onLoginConnection(_ sender: Any) {
+        print("hhh")
+        let 
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ SimpleTest/Auth/StartViewController.swift
@@ -0,0 +1,39 @@
+//
+//  StartViewController.swift
+//  SimpleTest
+//
+//  Created by Yupeng Lin on 7/3/18.
+//  Copyright © 2018 vluxe. All rights reserved.
+//
+
+import UIKit
+
+class StartViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    @IBAction func onLogin(_ sender: Any) {
+        print("login")
+        self.performSegue(withIdentifier: "LoginSelected", sender: self)
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
